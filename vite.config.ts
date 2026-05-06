7144f06ef222ae70807f85c9dc46d411d226ff7d
sgobbolo <146004638+sgobbolo@users.noreply.github.com>
2026-05-06 09:47:46 +0200

    Update API key in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({mode}) => {
   return {
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.AIzaSyCNcFAk6ct6TzUGPS1bvjw4F4onoEStzDg': JSON.stringify(env.AIzaSyCNcFAk6ct6TzUGPS1bvjw4F4onoEStzDg),
     },
     resolve: {
       alias: {
